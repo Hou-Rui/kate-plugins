f32f6ba4f74a6b830927984204a7cb570046f167
Hou Rui <houruinus@gmail.com>
2025-06-30 23:39:46 +0800

    fix code for KF6

--- KateBookmarksPlugin.cpp
+++ KateBookmarksPlugin.cpp
@@ -1,65 +1,98 @@
-#include "katebookmarksplugin.h"
+#include "KateBookmarksPlugin.hpp"
+
+#include <KPluginFactory>
+#include <KTextEditor/Application>
+#include <KTextEditor/Cursor>
 #include <KTextEditor/Document>
 #include <KTextEditor/Editor>
+#include <KTextEditor/MainWindow>
 #include <KTextEditor/View>
+#include <QIcon>
 #include <QListWidget>
 #include <QVBoxLayout>
+#include <QVariantMap>
 
-KateBookmarksPlugin::KateBookmarksPlugin(QObject *parent, const QList<QVariant> &)
-    : KTextEditor::Plugin((KTextEditor::Application *)parent), m_toolView(nullptr), m_listWidget(nullptr)
-{
-    createToolView();
-    refreshBookmarks();
-
-    // Connect signals for updates
-    connect(KTextEditor::Editor::instance(), &KTextEditor::Editor::editorStarted,
-            this, &KateBookmarksPlugin::refreshBookmarks);
-}
-
-void KateBookmarksPlugin::createToolView()
-{
-    m_toolView = mainWindow()->createToolView(this, QStringLiteral("kate_plugin_kate_bookmarks_view"), KTextEditor::MainWindow::Bottom, i18n("Project Bookmarks"));
-
-    m_listWidget = new QListWidget;
-    QVBoxLayout *layout = new QVBoxLayout;
-    layout->addWidget(m_listWidget);
-    m_toolView->setWidget(new QWidget);
-    m_toolView->widget()->setLayout(layout);
-
-    connect(m_listWidget, &QListWidget::itemDoubleClicked, this, &KateBookmarksPlugin::jumpToBookmark);
-}
-
-void KateBookmarksPlugin::refreshBookmarks()
-{
-    m_listWidget->clear();
-
-    auto documents = documentManager()->documents();
-    for (auto document : documents) {
-        if (auto view = mainWindow()->openUrl(document->url())) {
-            auto bookmarks = view->document()->bookmarks();
-            for (int bookmark : bookmarks) {
-                auto lineContent = view->document()->line(bookmark);
-                auto *item = new QListWidgetItem(QString("%1:%2 %3").arg(document->url().toLocalFile(), QString::number(bookmark + 1), lineContent));
-                item->setData(Qt::UserRole, QVariant::fromValue(document));
-                item->setData(Qt::UserRole + 1, bookmark);
-                m_listWidget->addItem(item);
-            }
-        }
-    }
-}
-
-void KateBookmarksPlugin::jumpToBookmark(QListWidgetItem *item)
-{
-    auto document = item->data(Qt::UserRole).value<KTextEditor::Document *>();
-    auto bookmark = item->data(Qt::UserRole + 1).toInt();
-    
-    if (auto view = mainWindow()->openUrl(document->url())) {
-        view->setCursorPosition(KTextEditor::Cursor(bookmark, 0));
-    }
-}
-
-KateBookmarksPlugin::~KateBookmarksPlugin() {}
 
 K_PLUGIN_CLASS_WITH_JSON(KateBookmarksPlugin, "katebookmarksplugin.json")
 
-#include "katebookmarksplugin.moc"
+KateBookmarksPlugin::KateBookmarksPlugin(QObject *parent, const QList<QVariant> &)
+    : KTextEditor::Plugin(parent)
+{
+}
+
+QObject *KateBookmarksPlugin::createView(KTextEditor::MainWindow *mainWindow)
+{
+    return new KateBookmarksView(this, mainWindow);
+}
+
+void KateBookmarksView::showMessage(const QString &msg)
+{
+    m_mainWindow->showMessage({
+        { QStringLiteral("category"), QStringLiteral("Bookmarks") },
+        { QStringLiteral("type"), QStringLiteral("Log") },
+        { QStringLiteral("text"), msg }
+    });
+}
+
+KateBookmarksView::KateBookmarksView(KateBookmarksPlugin *plugin, KTextEditor::MainWindow *mainWindow)
+    : m_mainWindow(mainWindow)
+{
+    showMessage("view init!");
+    m_toolView = m_mainWindow->createToolView(plugin,
+                                              "katebookmarksplugin",
+                                              KTextEditor::MainWindow::Left,
+                                              QIcon::fromTheme(QStringLiteral("bookmarks")),
+                                              tr("Bookmarks"));
+    auto layout = new QVBoxLayout(m_toolView);
+    m_listWidget = new QListWidget();
+    layout->addWidget(m_listWidget);
+
+    auto editor = KTextEditor::Editor::instance();
+    connect(editor, &KTextEditor::Editor::documentCreated, this, [this](auto, auto document) {
+        showMessage("Document created!");
+        connect(document, &KTextEditor::Document::markChanged, this, [this, document](auto, auto, auto) {
+            showMessage("Mark created!");
+            refreshBookmarks(document);
+        });
+        refreshBookmarks(document);
+    });
+    connect(m_listWidget, &QListWidget::itemDoubleClicked, this, &KateBookmarksView::jumpToBookmark);
+}
+
+void KateBookmarksView::refreshAllBookmarks()
+{
+    m_listWidget->clear();
+    auto documents = KTextEditor::Editor::instance()->documents();
+    for (auto document : documents) {
+        refreshBookmarks(document);
+    }
+}
+
+void KateBookmarksView::refreshBookmarks(KTextEditor::Document *document)
+{
+    auto marks = document->marks();
+    for (auto mark : marks) {
+        if (!(mark->type & KTextEditor::Document::Bookmark)) {
+            continue;
+        }
+        auto lineContent = document->line(mark->line);
+        auto itemContent = QString("%1: %2").arg(mark->line + 1).arg(lineContent);
+        auto *item = new QListWidgetItem(itemContent);
+        item->setData(Qt::UserRole, QVariant::fromValue(document));
+        item->setData(Qt::UserRole + 1, QVariant::fromValue(mark));
+        m_listWidget->addItem(item);
+    }
+}
+
+void KateBookmarksView::jumpToBookmark(QListWidgetItem *item)
+{
+    showMessage("Jump to Bookmark!");
+    auto document = item->data(Qt::UserRole).value<KTextEditor::Document *>();
+    auto mark = item->data(Qt::UserRole + 1).value<KTextEditor::Mark *>();
+
+    if (auto view = m_mainWindow->openUrl(document->url())) {
+        view->setCursorPosition(KTextEditor::Cursor(mark->line, 0));
+    }
+}
+
+#include "KateBookmarksPlugin.moc"
---
+++ KateBookmarksPlugin.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <QListWidget>
+#include <QObject>
+
+#include <KTextEditor/Document>
+#include <KTextEditor/Plugin>
+#include <KTextEditor/View>
+#include <KXMLGUIClient>
+
+class KateBookmarksPlugin : public KTextEditor::Plugin
+{
+    Q_OBJECT
+public:
+    explicit KateBookmarksPlugin(QObject *parent = nullptr, const QList<QVariant> & = QList<QVariant>());
+    QObject *createView(KTextEditor::MainWindow *mainWindow) override;
+};
+
+class KateBookmarksView : public QObject, public KXMLGUIClient
+{
+    Q_OBJECT
+public:
+    explicit KateBookmarksView(KateBookmarksPlugin *plugin, KTextEditor::MainWindow *mainWindow);
+
+public Q_SLOTS:
+    void refreshAllBookmarks();
+    void refreshBookmarks(KTextEditor::Document *document);
+    void jumpToBookmark(QListWidgetItem *item);
+
+private:
+    void showMessage(const QString &msg);
+
+    KTextEditor::MainWindow *m_mainWindow = nullptr;
+    QWidget *m_toolView = nullptr;
+    QListWidget *m_listWidget = nullptr;
+};
